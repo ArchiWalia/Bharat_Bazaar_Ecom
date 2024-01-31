ad7f441fda4eb74358f8fcb5ea90deb40d37fdd5
codeWodeWithArchi <archiwalia948@gmail.com>
2024-01-31 13:45:16 +0530

    final commit

---
+++ bharat/README.md
@@ -0,0 +1,44 @@
+## Project details 
+
+Install 
+- npm install -D tailwindcss
+- npx tailwindcss init
+
+
+<!-- css  -->
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+
+
+
+------------ Building parts ---------------
+1. First we build header part
+
+
+// 31:20
+npm i react-router-dom
+
+
+<!--  for api  -->
+https://fakestoreapiserver.reactbd.com/products
+
+npm i axios
+
+<!-- 1:21:00 -->
+
+-- use redux toolkit 
+npm install @reduxjs/toolkit react-redux
+
+
+npm i react-toastify
+
+<!-- 2:03:20 -->
+
+npm i redux-persist
+
+<!-- login -->
+npm install firebase
+
+npm i react-stripe-checkout
---
+++ bharat/src/firebase.config.js
@@ -0,0 +1,37 @@
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAxJVPPfR2G_IEV_VCXChBKcsQEfDoFdGA",
+//   authDomain: "bharat-bazar-c2722.firebaseapp.com",
+//   projectId: "bharat-bazar-c2722",
+//   storageBucket: "bharat-bazar-c2722.appspot.com",
+//   messagingSenderId: "434700473386",
+//   appId: "1:434700473386:web:239fe308d334da0aa71f53"
+// };
+
+// Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBG90_IKMLBKkrcMkv6n7APTrrz1B6c2dM",
+  authDomain: "ecommerce-40869.firebaseapp.com",
+  projectId: "ecommerce-40869",
+  storageBucket: "ecommerce-40869.appspot.com",
+  messagingSenderId: "413681319428",
+  appId: "1:413681319428:web:7aade5db92854c66a3bf51"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
